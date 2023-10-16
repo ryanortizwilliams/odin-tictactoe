94a12c9a8a81e7d33a7f80b84452b4e72c91dca5
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-10-16 15:43:16 +0900

    Add UI sections

--- main.js
+++ main.js
@@ -5,69 +5,68 @@ function createPlayer(symbol) {
   selectedCells = [];
   return { symbol, selectedCells };
 }
-
-playerX = createPlayer("X");
-playerO = createPlayer("O");
-
-const gameState = {
-  board: ["", "", "", "", "", "", "", "", ""],
-  currentPlayer: playerX,
-  winner: null,
-  gameOver: false,
-  winStates: [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ],
-  checkWinner: function () {
-    for (i = 0; i < this.winStates.length; i++) {
-      const winState = this.winStates[i];
-      if (
-        this.currentPlayer.symbol === this.board[winState[0]] &&
-        this.board[winState[0]] === this.board[winState[1]] &&
-        this.board[winState[1]] === this.board[winState[2]]
-      ) {
-        this.winner = this.currentPlayer;
-        console.log(this.winner);
+const playerX = createPlayer("X");
+const playerO = createPlayer("O");
+function playGame() {
+  const gameState = {
+    board: ["", "", "", "", "", "", "", "", ""],
+    currentPlayer: playerX,
+    winner: null,
+    gameOver: false,
+    winStates: [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ],
+    checkWinner: function () {
+      for (i = 0; i < this.winStates.length; i++) {
+        const winState = this.winStates[i];
+        if (
+          this.currentPlayer.symbol === this.board[winState[0]] &&
+          this.board[winState[0]] === this.board[winState[1]] &&
+          this.board[winState[1]] === this.board[winState[2]]
+        ) {
+          this.winner = this.currentPlayer;
+          alert(`${this.currentPlayer.symbol} is the winner!`);
+        }
       }
-    }
-  },
-};
-//create divs for the gameboard
+    },
+  };
 
-for (let i = 0; i < 9; i++) {
-  const gameBoard = document.querySelector(".game-board");
+  //create divs for the gameboard
 
-  const boardCell = document.createElement("div");
-  boardCell.addEventListener("click", function (e) {
-    if (!boardCell.firstChild) {
-      const symbol = document.createElement("h1");
-      symbol.innerText = gameState.currentPlayer.symbol;
-      e.target.appendChild(symbol);
-      addSymbol(i);
-    }
-  });
-  gameBoard.appendChild(boardCell);
-}
+  for (let i = 0; i < 9; i++) {
+    const gameBoard = document.querySelector(".game-board");
 
-//function for adding a symbol
-
-function addSymbol(index) {
-  gameState.board[index] = gameState.currentPlayer.symbol;
-  gameState.currentPlayer.selectedCells.push(index);
-
-  gameState.checkWinner();
-
-  if (gameState.currentPlayer === playerX) {
-    gameState.currentPlayer = playerO;
-  } else {
-    gameState.currentPlayer = playerX;
+    const boardCell = document.createElement("div");
+    boardCell.addEventListener("click", function (e) {
+      if (!boardCell.firstChild) {
+        const symbol = document.createElement("h1");
+        symbol.innerText = gameState.currentPlayer.symbol;
+        e.target.appendChild(symbol);
+        addSymbol(i);
+      }
+    });
+    gameBoard.appendChild(boardCell);
   }
 
-  //check if this function works
+  function addSymbol(index) {
+    gameState.board[index] = gameState.currentPlayer.symbol;
+    gameState.currentPlayer.selectedCells.push(index);
+
+    gameState.checkWinner();
+
+    if (gameState.currentPlayer === playerX) {
+      gameState.currentPlayer = playerO;
+    } else {
+      gameState.currentPlayer = playerX;
+    }
+  }
 }
+
+playGame();
